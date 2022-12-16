534bf3e8c3b789c00254254bb21f47413c3ee55c
MarieAkinyi <mr1316280@gmail.com>
2022-12-16 02:41:32 -0800

    weee

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," followed by a new line
- * Return: Always o
+ * main - prints "with proper grammar,
+ * but the outcome is a piece of art,"
+ * followed by a new line
+ * Return: o
  */
 int main(void)
 {
