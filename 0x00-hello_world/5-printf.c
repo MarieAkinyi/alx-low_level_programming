a2576b99113b1b6e4bbc6cfb04873df074189bb9
MarieAkinyi <mr1316280@gmail.com>
2022-12-16 02:36:09 -0800

    weee

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art," followed by a new line
+ * Retun: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
