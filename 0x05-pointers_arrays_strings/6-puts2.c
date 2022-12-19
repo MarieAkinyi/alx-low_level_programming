2e304ceb9dee9066fdd77a8bcb915c9871e50368
MarieAkinyi <mr1316280@gmail.com>
2022-12-19 01:06:42 -0800

    hey

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,25 +10,23 @@
 void puts2(char *str)
 
 {
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-		int longi = 0;
-		int t = 0;
-		char *y = str;
-		int o;
-
-		while (*y != '\0')
-
-		{											y++;
-			longi++;
-		}
-		t = longi - 1;
-		for (o = 0 ; o <= t ; o++)
-		{
-	        	if (o % 2 == 0)
-												{
-													_putchar(str[o]);
-												}
-		}
+	while (*y != '\0')
+	{											y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+       		if (o % 2 == 0)	
+	{
+												_putchar(str[o]);
+	}
+	}
 		_putchar('\n');
 
 }
