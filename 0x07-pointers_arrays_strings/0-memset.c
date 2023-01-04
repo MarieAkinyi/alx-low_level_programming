eb3f7e4422345aa110e578ffd2da2222087a8a48
MarieAkinyi <mr1316280@gmail.com>
2023-01-04 04:16:30 -0800

    2023

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,27 +1,26 @@
-/*
- * file: 0-memset.c
- * author:marie
+/*file:0-memset.c
+ * ath:not
  */
 #include "main.h"
 
 /**
- * _memset: fills the first n character of the memory
- *  pointed to by @s with the constant byte @c.
- *  @s: A pointer to the memory area to be filled.
- *  @c: character to fill the memory area with
- *  @n: number of bytes to be filled
- *  description_memset: over there
- *  return: pointer to the meory area @s
+ * _memset - Fills the first n bytes of the memory area
+ *  pointed to by @s with the constant byte @c
+ *  @s: A pointer to the memory area to be filled
+ *  @c: The character to fill the memory area with.
+ *  @n: The number of bytes to be filled
+ *  description:memset over there
+ *  Return: A pointer to the filled memory area @s.
  */
+
 void *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
 	unsigned char *memory = s, value = c;
 
+	for(index = 0; index < n; index++)
+		memory[index] = value;
 
-	for (index = 0; index < n; index++)
-	       memory[index] = value;
 
-return(memory);
+	return(memory);
 }
-
--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- *  _putchar - writes the character c to stdou
+ *  _putchar - writes the character c to stdout
  *  @c: the character to print
- *  return:on success 1.
+ *  return:on sucess 1.
  *  On error, -1 is returned, and errno is set appropriately.
  */
 
-int _putchar(char c)
+void _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return(write 1, &c, 1));
 }
